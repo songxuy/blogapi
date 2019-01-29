3e638a35b9c5ef5a14ab10c4d332bc3101eb0bba
songxuy <541046145@qq.com>
2019-01-29 22:47:36 +0800

    add

--- server/models/user.js
+++ server/models/user.js
@@ -4,7 +4,17 @@ const Promise = require('bluebird')
 
 const UserSchema = new Schema({
     username: String,
+    uid: String,
     email: String,
+    sex: { type: String, default: '' },
+    articalNum: { type: Number, default: 0 },
+    starNum: { type: Number, default: 0 },
+    totalRead: { type: Number, default: 0 },
+    commentNum: { type: Number, default: 0 },
+    followeeNum: { type: Number, default: 0 },
+    followerNum: { type: Number, default: 0 },
+    tag: { type: Array, default: [] },
+    mysave: { type: Array, default: [] },
     password: String,
     creat_date: String,
     update_date: String,
